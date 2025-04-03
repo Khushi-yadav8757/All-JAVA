24f82b767b7bb222039a44cf267307b87caf6ecb
Khushi Yadav <kky957242@gmail.com>
2025-04-03 23:45:36 +0530

    Create new.java

---
+++ new.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello friends ");
+    System.out.println("Lets learn Java.");
+    System.out.println("It is awesome!");
+  }
+}
