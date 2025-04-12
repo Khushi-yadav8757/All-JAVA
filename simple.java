aed9b27cc43e9de9e7f45f22c8953c5ead2def66
Khushi Yadav <kky957242@gmail.com>
2025-04-12 23:11:05 +0530

    Create simple.java

---
+++ simple.java
@@ -0,0 +1,9 @@
+class A1
+{
+              public static void main(String args[])
+                {
+                       System.out.println("Hello K");  
+                       System.out.println("Hello S");
+                       System.out.println("Hello R");
+                 }
+}
