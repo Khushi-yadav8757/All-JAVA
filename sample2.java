3b5946f1115f0334ef730130d7d325f18c6fbe56
Khushi Yadav <kky957242@gmail.com>
2025-04-12 23:11:59 +0530

    Create sample2.java

---
+++ sample2.java
@@ -0,0 +1,14 @@
+class A2
+{
+         public static void main(String args[])
+         {
+            System.out.println("Hello");
+            System.out.println("Khushi");
+            System.out.println(27-10-2006);
+            System.out.println(15.23);
+            System.out.println(23*2);
+            System.out.println(27+12);
+
+         }
+
+}
