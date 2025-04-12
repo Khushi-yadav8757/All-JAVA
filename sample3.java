d1b6483a1c6b46eac179afe96e16674aa786bfb3
Khushi Yadav <kky957242@gmail.com>
2025-04-12 23:12:36 +0530

    Create sample3.java

---
+++ sample3.java
@@ -0,0 +1,12 @@
+class A3
+{ 
+            public static void main(String args[])
+                    {
+                          int x = 123;
+                          double y = 34.76;
+                          System.out.println(x);
+                          System.out.println(y);
+                          System.out.println(x+y);
+                          System.out.println("Value of x is "+x);
+                          }
+}
