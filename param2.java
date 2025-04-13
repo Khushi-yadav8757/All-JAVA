3f2acdbbc10764808a9c6cc9fc4fc8f2030b4669
Khushi Yadav <kky957242@gmail.com>
2025-04-13 20:20:00 +0530

    Create param2.java

---
+++ param2.java
@@ -0,0 +1,11 @@
+public class Main {
+  static void myMethod(String fname, int age) {
+    System.out.println(fname + " is " + age);
+  }
+
+  public static void main(String[] args) {
+    myMethod("Karthik",25);
+    myMethod("Sana", 24);
+    myMethod("Rishi", 5);
+  }
+}
