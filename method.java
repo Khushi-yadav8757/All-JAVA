d7afd958bb349fe9325e24886fe9676b831b52ce
Khushi Yadav <kky957242@gmail.com>
2025-04-13 20:07:55 +0530

    Create method.java

---
+++ method.java
@@ -0,0 +1,17 @@
+public class Main {
+  static void myMethod() {
+    System.out.println("Coder Khushi");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();      // you can called method multiple timesss..
+    myMethod();
+  }
+}
+
+
+//output will be.....
+ Coder Khushi
+ Coder Khushi
+ Coder Khushi
