29461da4f3a915cbb6e5487ebfcd2e88f2b53163
Khushi Yadav <kky957242@gmail.com>
2025-04-13 20:39:36 +0530

    Create returnval.java

---
+++ returnval.java
@@ -0,0 +1,13 @@
+public class Main {
+  static int myMethod(int x) {
+    return 7 + x;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(myMethod(20));
+  }
+}
+
+
+output will be...
+    27
