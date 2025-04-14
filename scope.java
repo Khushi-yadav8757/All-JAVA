0cd2c562395157b6d234d54df41cdf905df74332
Khushi Yadav <kky957242@gmail.com>
2025-04-14 22:47:57 +0530

    Create scope.java

---
+++ scope.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // Code here cannot use x
+
+    int x = 2712;
+
+    // Code here can use x
+    System.out.println(x);
+  }
+}
+
+
+output will be....
+  2712
