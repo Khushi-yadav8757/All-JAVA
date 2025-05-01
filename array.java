d8fb4753ea6baa45b9633871cf3034fe28c87561
Khushi Yadav <kky957242@gmail.com>
2025-05-01 22:26:11 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+public class App{
+    public static void main(String[]args)throws Exception{
+        int a[]={33,3,4,5};
+       // System.out.println(a[0]);
+        for(int i = 0; i< a.length;i++)
+           System.out.println(a[i]);
+
+
+    }
+}
