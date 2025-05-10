94235ab577dc61d6ac8d68c45c4360d2ac385ffb
Khushi Yadav <kky957242@gmail.com>
2025-05-10 23:21:34 +0530

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,16 @@
+public class ArrayPrint{
+  public static void main(String[]args){
+  int arr[]={10,20,30,40,50};
+    for(int i=0;i<arr.length;i++){
+      System.out.println("Element at index"+i+":"+arr[i]);
+    }
+  }
+}
+
+// so output will be...   
+/* Element at index 0: 10  
+Element at index 1: 20  
+Element at index 2: 30  
+Element at index 3: 40  
+Element at index 4: 50 */
+
