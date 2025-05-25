c3f03012c3ce9c2d60bbf312ddf7f464ba18092d
Khushi Yadav <kky957242@gmail.com>
2025-05-25 20:09:01 +0530

    Create poly.java

---
+++ poly.java
@@ -0,0 +1,23 @@
+// Overloading
+class Add {
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+// Overriding
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
