b92050625c5f91f846563b473b3dff8bd1fb60dc
Khushi Yadav <kky957242@gmail.com>
2025-05-28 21:08:44 +0530

    Create elseif.java

---
+++ elseif.java
@@ -0,0 +1,9 @@
+int marks = 75;
+
+if (marks >= 90) {
+    System.out.println("Grade: A");
+} else if (marks >= 75) {
+    System.out.println("Grade: B");
+} else {
+    System.out.println("Grade: C");
+}
