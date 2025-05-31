8c9e543be805559e139c9b2b380aa3fee28c1b18
Khushi Yadav <kky957242@gmail.com>
2025-05-31 21:16:44 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,12 @@
+//product of a and b
+import java.util.*;
+public class JavaBasics{
+  public static void main(String args[]){
+    Scanner sc =new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    //product
+  int product = a * b;
+    System.out.print("product");
+  }
+}
