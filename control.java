15c04987dae1def5da2cb394226d3e0924c14409
Khushi Yadav <kky957242@gmail.com>
2025-06-17 18:35:16 +0530

    Create control.java

---
+++ control.java
@@ -0,0 +1,17 @@
+public class ControlFlowExample {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        int day = 2;
+        switch(day) {
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            default: System.out.println("Other Day");
+        }
+    }
+}
