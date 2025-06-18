d4c15a0e47fa8a4ae2aa9f01b4d29f18ed1a1d68
Khushi Yadav <kky957242@gmail.com>
2025-06-18 16:35:06 +0530

    Create final.java

---
+++ final.java
@@ -0,0 +1,7 @@
+//What is the use of final, finally, and finalize() in Java?
+final: keyword to declare constants, prevent method/variable inheritance.
+
+finally: block used in exception handling, always executes.
+
+finalize(): method called by garbage collector before destroying the object.
+
