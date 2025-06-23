73599472e8c884963a705f9259e6141bf18b1e8d
Khushi Yadav <kky957242@gmail.com>
2025-06-23 17:18:58 +0530

    Create difference.java

---
+++ difference.java
@@ -0,0 +1,6 @@
+ Q3. What is the difference between Overloading and Overriding?
+| Feature    | Overloading                                | Overriding                                   |
+| ---------- | ------------------------------------------ | -------------------------------------------- |
+| Definition | Same method name with different parameters | Same method name & parameters in child class |
+| Occurs in  | Same class                                 | Parent-child classes                         |
+| Binding    | Compile time                               | Runtime                                      |
