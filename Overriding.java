7ecd30f7fe8b38e3591d41a9a1a74399f407abdf
Khushi Yadav <kky957242@gmail.com>
2025-06-23 17:16:37 +0530

    Create Overriding.java

---
+++ Overriding.java
@@ -0,0 +1,11 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Meow");
+    }
+}
