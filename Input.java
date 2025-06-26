5b6991ca9999210f84870b5e78504ffe7841c434
Khushi Yadav <kky957242@gmail.com>
2025-06-26 09:18:08 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class WelcomeUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();  // Takes full line input
+
+        System.out.println("Welcome, " + name + "!");
+
+        sc.close();
+    }
+}
