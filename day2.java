a73114233917d5feca07715bf84d1bd2fa3fe0d9
Khushi Yadav <kky957242@gmail.com>
2025-07-20 19:15:44 +0530

    Update day2.java

--- day2.java
+++ day2.java
@@ -1,4 +1,10 @@
-public class ArrayPrint{
+//find elements through index 
+  
+  
+  
+  
+  
+ public class ArrayPrint{
   public static void main(String[]args){
   int arr[]={10,20,30,40,50};
     for(int i=0;i<arr.length;i++){
@@ -6,6 +12,9 @@ public class ArrayPrint{
     }
   }
 }
+
+
+
 
 // so output will be...   
 /* Element at index 0: 10  
