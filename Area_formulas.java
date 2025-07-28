163bc0b49a8b7bde09aff25d5c4ef96e10cd1081
Khushi Yadav <kky957242@gmail.com>
2025-07-28 20:36:33 +0530

    Create Area_formulas.java

---
+++ Area_formulas.java
@@ -0,0 +1,13 @@
+       double radius = 7;
+        int length = 10, width = 5;
+        double base = 6, height = 4;
+
+        double areaCircle = Math.PI * radius * radius;
+        int areaRectangle = length * width;
+        double areaTriangle = 0.5 * base * height;
+
+        System.out.println("Area Calculations:");
+        System.out.println("Circle: " + areaCircle);
+        System.out.println("Rectangle: " + areaRectangle);
+        System.out.println("Triangle: " + areaTriangle);
+        System.out.println("-----------------------------------");
