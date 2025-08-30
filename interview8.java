0eb9f9f6ba9084b70a42957c294333610eb7645b
Khushi Yadav <kky957242@gmail.com>
2025-08-30 14:16:02 +0530

    Update interview8.java

--- interview8.java
+++ interview8.java
@@ -12,7 +12,6 @@ public class DuplicateChars {
                 System.out.println(ch + ": " + map.get(ch));
         }
     }
-
     public static void main(String[] args) {
         findDuplicates("programming");
     }
