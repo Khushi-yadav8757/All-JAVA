f2e0e59422f16d1e2f08cafe98103d4a6cbbe832
Khushi Yadav <kky957242@gmail.com>
2025-10-04 01:19:35 +0530

    Create JavaOperat.java

---
+++ JavaOperat.java
@@ -0,0 +1,11 @@
+public class JavaOperat {
+    public static void main(String[] args) {
+        int A = 100;
+        int B = 25;
+        System.out.println("Addition :"+(A+B));
+        System.out.println("Substraction :"+(A-B));
+        System.out.println("Multiply :"+(A*B));
+        System.out.println("Divide :"+(A/B));
+        System.out.println("Modulo :"+(A%B));
+    }
+}
