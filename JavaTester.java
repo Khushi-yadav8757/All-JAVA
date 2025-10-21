b4086b567274baef48ae7f9282869d113569679c
Khushi Yadav <kky957242@gmail.com>
2025-10-21 19:37:01 +0530

    Create JavaTester.java

---
+++ JavaTester.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JavaTester {
+   public static void main(String args[]) {
+      String a, b;
+      Scanner scanner = new Scanner(System.in); 
+
+      System.out.println("Enter The value for variable a");
+      a = scanner.nextLine();
+
+      System.out.println("Enter The value for variable b");
+      b = scanner.nextLine();
+
+      System.out.println("The value you have entered for a is " + a);
+
+      System.out.println("The value you have entered for b is " + b);
+
+      scanner.close();
+   }	   
+}
