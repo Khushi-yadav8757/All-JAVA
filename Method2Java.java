acbf4eb594b0dcc155d01ab6e31e3f4878a5d661
Khushi Yadav <kky957242@gmail.com>
2025-11-19 19:33:02 +0530

    Create  Method2Java.java

---
+++ Method2Java.java
@@ -0,0 +1,18 @@
+public class Method2Java {
+    public static void main(String[] args) {
+        rishika();
+    }
+    public static void rishika(){
+        System.out.println("archit");
+        ishan();
+    }
+    public static void ishan(){
+        System.out.println("Lakshay");
+        Virat();
+    }
+    public static void Virat(){
+        System.out.println("Malini");
+        rishika();
+    }
+        
+}
