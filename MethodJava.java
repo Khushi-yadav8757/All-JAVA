6b3391a729da98bb5d85d7bd669d1e3a3140432a
Khushi Yadav <kky957242@gmail.com>
2025-11-19 19:27:14 +0530

    Create MethodJava.java

---
+++ MethodJava.java
@@ -0,0 +1,19 @@
+// Methods
+
+Package Methods;
+ public class method1Java {
+    public static void main(String[] args) {
+        Sanju();
+    }
+    public static void Sanju(){
+        System.out.println("Khushi");
+        Karthik();
+    }
+    public static void Karthik(){
+        Rishi();
+        System.out.println("Sanjana");
+    }
+    public static void Rishi(){
+        System.out.println("Sanju");
+    }
+}
