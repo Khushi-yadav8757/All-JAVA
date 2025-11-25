dd4bb049ea6c5949ae77234c0bbc699c32b19805
Khushi Yadav <kky957242@gmail.com>
2025-11-25 23:21:13 +0530

    Create  ArrayBasicc.java

---
+++ ArrayBasicc.java
@@ -0,0 +1,11 @@
+public class ArrayBasicc {
+    public static void main(String[] args) {
+    int [ ] arr={12,27,-24,15,20};
+    for(int i=0;i<=5;i++){
+  System.out.println(arr[i]+" ");
+   }
+}
+
+   
+//int []arr =new int[4]; // u can print 4 size of array/ 0 to 3          }
+}
