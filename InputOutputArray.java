5bfdae06eccfb303e63c622239602f9487142994
Khushi Yadav <kky957242@gmail.com>
2025-11-25 23:16:41 +0530

    Create InputOutputArray.java

---
+++ InputOutputArray.java
@@ -0,0 +1,14 @@
+public class InputOutputArray {
+    public static void main(String[] args) {
+        
+        int []arr={5,-8,2,27,12,-24,77,98,44};
+        int n=arr.length; // index 0 to n-1
+        for(int i=0;i<=n-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+        int[] khushi = new int[7];
+        for(int i=0;i<7;i++){
+            System.out.print(khushi[i]+" ");
+        }
+    }
+}
